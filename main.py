e5d7dd1b4f8aca7af3efc11810fafa79acb604d1
hirokisekine <hiroki.sekine@gmail.com>
2026-09-04 18:39:49 +0900

    main.pyを更新し、requirements.txtを追加

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from factory import ItemFactory
 from item import GiftWrapDecorator, MessageCardDecorator
-"""
+#"""
 apple = ItemFactory.create_item("perishable", "りんご", 100, 10, "2026-09-10")
 wrapped_apple = GiftWrapDecorator(apple)
 special_apple = MessageCardDecorator(wrapped_apple)  # ラッピング + メッセージカード
 
-print(special_apple)                          # ?
-print(special_apple.get_discounted_price())    # ?
+print(special_apple)                          
+print(special_apple.get_discounted_price())
 
 # Inventoryに追加しても、普通のItemと同じように扱えることを確認
 from inventory import Inventory
@@ -14,6 +14,7 @@ inventory = Inventory()
 inventory.add_item(special_apple)
 inventory.show_all()
 inventory.show_discounted_prices()
+
 """
 from unittest.mock import Mock
 
@@ -21,5 +22,6 @@ fake_notifier = Mock()
 fake_notifier.update("テストメッセージ")
 
 # 本当にupdateが呼ばれたか確認
-print(fake_notifier.update.called)              # ?
-print(fake_notifier.update.call_args)            # ?
+print(fake_notifier.update.called)
+print(fake_notifier.update.call_args)
+"""
